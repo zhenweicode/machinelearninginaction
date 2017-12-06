af4d67179da2c61032dccce3fae5c074acad77c7
xiezhenwei <xiezhenwei@meituan.com>
2017-12-06 19:16:22 +0800

    简单注释

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -1,36 +1,49 @@
-'''
-Created on Sep 16, 2010
-kNN: k Nearest Neighbors
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
-            k: number of neighbors to use for comparison (should be an odd number)
-            
-Output:     the most popular class label
+"""
+k近邻算法
 
-@author: pbharrin
-'''
-from numpy import *
-import operator
+@Author: xie
+@Date: 2017/12/5
+"""
+
+from numpy import *   #科学计算包
+import operator       #运算符模块
 from os import listdir
 
+"""
+作用：使用k近邻算法将每组数据划分到某个类中
+其伪代码如下：
+对未知类别属性的数据集中的每个点依次执行以下操作：
+(1) 计算已知类别数据集中的点与当前点之间的距离；
+(2) 按照距离递增次序排序；
+(3) 选取与当前点距离最小的k个点；
+(4) 确定前k个点所在类别的出现频率；
+(5) 返回前k个点出现频率最高的类别作为当前点的预测分类。
+
+参数：
+inX：用于分类的输入向量
+dataSet：输入的训练样本集，行数是数据的个数，列数是数据集特征值的个数
+labels：标签向量，即分类的数目，标签向量的元素数目和矩阵dataSet的行数相同
+k：用于选择最近邻居的数目
+"""
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
-    sqDiffMat = diffMat**2
+    #距离计算
+    dataSetSize = dataSet.shape[0]   #NumPy中的shape方法用来查看矩阵或者数组的维数
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
-    classCount={}          
+    distances = sqDistances ** 0.5
+
+    #选择距离最小的k个点
+    sortedDistIndicies = distances.argsort()  #argsort()方法得到矩阵中每个元素的排序序号
+    classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-        
-        #if classCount.has_key(voteIlabel):
-        #    classCount[voteIlabel] += 1
-        #else:
-        #    classCount[voteIlabel] = 1
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    #排序
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
